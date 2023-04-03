2cbe59af13a7534383628a391ea4d2f2ff2da604
zeynab81 <mamakhovazeynab@gmail.com>
2023-04-02 22:28:45 -0400

    Group1 is created and ready deployed

---
+++ src/FirstPractice.java
@@ -0,0 +1,10 @@
+public class FirstPractice {
+    public static void main(String[] args) {
+
+        String name;
+        String age;
+        String city;
+
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] groupNames = {"Artem", "Aigerim", "Diego"};
+        System.out.println("Please enter your name here");
+        System.out.println("My name is " + groupNames[1]);
+
+    }
+}
